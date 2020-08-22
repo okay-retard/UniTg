acb04f4c7ba7921896b1299a25a94e63291081d8
shreyansh8184 <54529384+shreyansh8184@users.noreply.github.com>
2020-08-22 16:38:40 +0530

    Update pmpermit.py

--- stdplugins/pmpermit.py
+++ stdplugins/pmpermit.py
@@ -17,8 +17,14 @@ borg.storage.PREV_REPLY_MESSAGE = {}
 
 BAALAJI_TG_USER_BOT = "My Master hasn't approved you to PM."
 TG_COMPANION_USER_BOT = "Please wait for his response and don't spam his PM."
-UNIBORG_USER_BOT_WARN_ZERO = "I am currently offline. Please do not SPAM me."
-UNIBORG_USER_BOT_NO_WARN = (
+if Config.BLOCK_MESSAGE:
+    UNIBORG_USER_BOT_WARN_ZERO = BLOCK_MESSAGE
+else:
+    UNIBORG_USER_BOT_WARN_ZERO = "I am currently offline. Please do not SPAM me."
+if Config.CUSTOM_PM:
+    UNIBORG_USER_BOT_NO_WARN = Custom_PM
+else:
+    UNIBORG_USER_BOT_NO_WARN = (
     "`Hello! This is an automated message.\n\n`"
     "`I haven't approved you to PM yet.`"
     "`Please wait for me to look in, I mostly approve PMs.\n\n`"

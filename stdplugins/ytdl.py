018754610a33b11f97b59c54b631f0196e18667b
shreyansh8184 <54529384+shreyansh8184@users.noreply.github.com>
2020-08-22 18:39:44 +0530

    Update ytdl.py

--- stdplugins/ytdl.py
+++ stdplugins/ytdl.py
@@ -216,11 +216,16 @@ async def download_video(v_url):
         await v_url.delete()
         os.system(" youtube-dl --rm-cache-dir")
 
-@borg.on(admin_cmd(pattern="song (.*)"))
+@borg.on(admin_cmd("song ?(.*)")
 async def _(event):
     song = url = event.pattern_match.group(1) + " " + "song"
+    if not song:
+        await event.edit("`Enter song name`")
+        return
+    await event.edit("Processing..."
     os.system(f"youtube-dl -x --audio-format mp3 --add-metadata --embed-thumbnai 'ytsearch:{song}'")
     l = glob.glob("*.mp3")
     if not l:
         await event.edit("`Song not found`")
+        return
     await event.client.send_file(event.chat_id, l, supports_streaming=True, reply_to=event.message)

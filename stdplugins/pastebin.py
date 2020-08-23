5201fb07661fa4ef426178cb771872b5a4d7d749
shreyansh8184 <54529384+shreyansh8184@users.noreply.github.com>
2020-08-23 16:49:18 +0530

    Update pastebin.py

--- stdplugins/pastebin.py
+++ stdplugins/pastebin.py
@@ -7,7 +7,24 @@ import asyncio
 from uniborg.util import admin_cmd
 import aiohttp
 
-from stdplugins.corona_virus import AioHttp
+class AioHttp:
+    @staticmethod
+    async def get_json(link):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(link) as resp:
+                return await resp.json()
+
+    @staticmethod
+    async def get_text(link):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(link) as resp:
+                return await resp.text()
+
+    @staticmethod
+    async def get_raw(link):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(link) as resp:
+                return await resp.read()
 
 @borg.on(admin_cmd("paste ?(.*)"))
 async def _(event):
